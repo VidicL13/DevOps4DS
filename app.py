e24fac7b0ec3e21044e8ad30084efa53c684a769
Luka Vidic <luka94vidic@gmail.com>
2024-03-10 21:26:08 +0100

    update path to model

--- app.py
+++ app.py
@@ -1,19 +1,9 @@
 from vetiver import VetiverModel
 import vetiver
 import pins
-import logging
-import os 
-   
-# Get the path of current working directory 
-path = os.getcwd() 
-   
-# Get the list of all files and directories 
-dir_list = os.listdir(path)
-
-logging.warning(dir_list)
 
 
-b = pins.board_folder('./data/model', allow_pickle_read=True)
+b = pins.board_folder('./app/data/model', allow_pickle_read=True)
 v = VetiverModel.from_pin(b, 'penguin_model')
 
 vetiver_api = vetiver.VetiverAPI(v)
