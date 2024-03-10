3f445dc5cf0bc8ec6c4fbd7092d46fe48c22a8ee
Luka Vidic <luka94vidic@gmail.com>
2024-03-10 19:42:40 +0100

    add test application

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pins
 
 
 b = pins.board_s3('do4ds-lab-bucket', allow_pickle_read=True)
-v = VetiverModel.from_pin(b, 'penguin_model', version = '20240104T210037Z-0f6c7')
+v = VetiverModel.from_pin(b, 'penguin_model')
 
 vetiver_api = vetiver.VetiverAPI(v)
 api = vetiver_api.app
