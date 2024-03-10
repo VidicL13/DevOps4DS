e13e1aab1aacc4ffe4f4dc934947be98a6f89b7e
Luka Vidic <luka94vidic@gmail.com>
2024-03-10 21:22:03 +0100

    add logger

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from vetiver import VetiverModel
 import vetiver
 import pins
+import logging
+import os 
+   
+# Get the path of current working directory 
+path = os.getcwd() 
+   
+# Get the list of all files and directories 
+dir_list = os.listdir(path)
+
+logging.warning(dir_list)
 
 
 b = pins.board_folder('./data/model', allow_pickle_read=True)
